ed004943d915a95e0c890a89fb4b3343714c4fc0
Cláudio Henrique <00claudio.henrique@gmail.com>
2020-07-13 10:26:24 -0300

    Update mint.sh

--- mint.sh
+++ mint.sh
@@ -6,26 +6,16 @@ mkdir Downloads/softwares && cd Downloads/softwares
 
 sudo apt update && sudo apt upgrade -y
 
-# Add Etcher (create bootable flashdrives) repo
-# echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-# sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
-
-# Import GPG keys into APT repo
-sudo -E apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 5044912E
-
 # Add VSCode repo
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 # Install and purge apt packages
-sudo apt update && sudo apt install -y snapd balena-etcher-electron terminator git gparted vim g++ gdb python3-pip openjdk-8-jdk* dropbox htop neofetch atril flatpak spotify-client fonts-firacode vlc apt-transport-https code
-sudo apt purge -y xplayer gimp system-config-printer tomboy simple-scan hexchat thunderbird xreader rhythmbox celluloid
+sudo apt update && sudo apt install -y balena-etcher-electron terminator git gparted vim g++ gdb python3-pip openjdk-8-jdk* dropbox htop neofetch atril flatpak spotify-client fonts-firacode vlc apt-transport-https code gimp telegram-desktop
+sudo apt purge -y xplayer system-config-printer tomboy simple-scan hexchat thunderbird xreader rhythmbox celluloid
 
-sudo apt install -y -f && sudo apt autoremove && sudo apt autoclean && sudo apt clean
-
-# Snap pkgs
-sudo snap install gimp telegram-desktop 
+sudo apt install -y -f && sudo apt autoremove && sudo apt autoclean && sudo apt clean 
 
 # Encryptr (password manager)
 wget -c http://www.spideroak.com/release/Encryptr/deb_x64
