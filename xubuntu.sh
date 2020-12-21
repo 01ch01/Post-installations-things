5fcb5c323e03e5c636c684327ec2072b77666400
Cláudio Henrique <00claudio.henrique@gmail.com>
2020-12-21 00:10:17 -0300

    Update xubuntu.sh

--- xubuntu.sh
+++ xubuntu.sh
@@ -6,7 +6,7 @@ mkdir Downloads/softwares $HOME/.themes && cd Downloads/softwares
 
 sudo apt update && sudo apt upgrade -y && sudo apt install -f -y
 
-sudo apt update && sudo apt install -y snapd synaptic terminator git htop vlc gparted vim g++ gufw python3-pip redshift-gtk gnome-disk-utility fonts-firacode telegram-desktop balena-etcher-electron
+sudo apt update && sudo apt install -y snapd synaptic terminator git htop vlc gparted vim g++ gufw python3-pip redshift-gtk gnome-disk-utility fonts-firacode telegram-desktop balena-etcher-electron mysql-server
 
 sudo snap install spotify gimp discord && sudo snap install code --classic
 
@@ -15,35 +15,26 @@ sudo apt purge -y parole pidgin gimp system-config-printer xfburn simple-scan gn
 cd ~/Downloads/softwares
 
 wget -c https://codeload.github.com/mantissa-/mantis-theme/zip/master && unzip master
-
 cd mantis-theme-master
-
 sudo mv Mantis 'Mantis Dusk' 'Mantis Night' /usr/share/themes/
-
 cd ..
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
 sudo dpkg -i google-chrome-stable_current_amd64.deb 
 
 git config --global user.name "01ch01"
-
 git config --global user.email "00claudio.henrique@gmail.com"
-
 git config --global color.ui auto
 
 wget -c https://github.com/PapirusDevelopmentTeam/papirus-icon-theme/archive/master.zip
-
 unzip master.zip
-
 cd papirus-icon-theme-master
-
 sudo mv Papirus* ePapirus/ /usr/share/icons/
-
 gtk-update-icon-cache /usr/share/icons/ePapirus/
-
 gtk-update-icon-cache /usr/share/icons/Papirus/
-
 gtk-update-icon-cache /usr/share/icons/Papirus-Dark/
-
 gtk-update-icon-cache /usr/share/icons/Papirus-Light/
+
+wget -c https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo dpkg -i teamviewer_amd64.deb
+sudo apt install -f -y
